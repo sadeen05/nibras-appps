9ab1c87ec58974b8b96546c380ef579091b7c139
sadeen05 <167700750+sadeen05@users.noreply.github.com>
2026-04-26 19:41:37 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello from Nibras App! The server is live and working perfectly.');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
